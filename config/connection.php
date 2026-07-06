2ed2e56aa1fd237a14641795b1497e5147ef4218
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-06 13:39:59 +0530

    debug

--- config/connection.php
+++ config/connection.php
@@ -15,8 +15,10 @@ $dbHost = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
 $dbUser = $_ENV['DB_USER'] ?? getenv('DB_USER');
 $dbPass = $_ENV['DB_PASS'] ?? getenv('DB_PASS');
 $dbName = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
+$dbPort = $_ENV['DB_PORT'] ?? getenv('DB_PORT') ?? 3306;
 
-$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName, (int)$dbPort);
 
 if (!$conn) {
     error_log("DB Connection failed: " . mysqli_connect_error());
