dcdbe693fc95531a969b2574f32f6c579c2eda38
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-06 15:42:11 +0530

    test

--- php/dashboard_data.php
+++ php/dashboard_data.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 session_start();
 
-require_once "includes/auth_check.php";
+require_once "../includes/auth_check.php";
 
 if(!isset($_SESSION['user_id'])){
     header("location: ../index.php");
