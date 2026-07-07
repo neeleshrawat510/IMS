644a19d642033af7dd5fe95c1c0cb148ffd172b1
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-07 09:39:33 +0530

    remove session & cookie

--- logout.php
+++ logout.php
@@ -5,6 +5,26 @@ session_start();
 //connection setup
 include("config/connection.php");
 
+//remove refresh token from db
+If (isset($_SESSION['user_id'])){
+    $userId = $_SESSION['user_id'];
+
+    mysqli_query($conn, "UPDATE users SET refresh_token = NULL, refresh_token_expires_at = NULL 
+                                WHERE id='$userId'");
+}
+
+
+//destroy everthing stored in session
+$_SESSION =[];
+
 session_destroy();
 
+
+//delete JWT cookie
+
+setcookie(
+    "auth_token", "", time() -3600, "/"
+);
+
+echo "success";
 ?>
