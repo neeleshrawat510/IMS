fd7657df8f24027c0383f235c86cce901b718d85
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-08 08:36:47 +0530

    testing

--- php/new_password.php
+++ php/new_password.php
@@ -1,5 +1,8 @@
 <?php
 //connection setup
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include("../config/connection.php");
 $token = $_POST['token'];
