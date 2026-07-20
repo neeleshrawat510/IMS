7fe9612601f9944eb35ffec895ef70c84279d169
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-20 10:54:08 +0530

    created webhook page

---
+++ webhook.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'vendor/autoload.php';
+include 'config/connection.php';
+
+use Stripe\Webhook;
+
+// Read raw request body
+$payload = @file_get_contents('php://input');
+
+// Stripe Signature
+$sigHeader = $_SERVER['HTTP_STRIPE_SIGNATURE'] ?? '';
+
+// Webhook Secret
+$endpointSecret = getenv('STRIPE_WEBHOOK_SECRET');
+
+try {
+
+    $event = Webhook::constructEvent(
+        $payload,
+        $sigHeader,
+        $endpointSecret
+    );
+
+} catch (\UnexpectedValueException $e) {
+
+    http_response_code(400);
+    exit("Invalid Payload");
+
+} catch (\Stripe\Exception\SignatureVerificationException $e) {
+
+    http_response_code(400);
+    exit("Invalid Signature");
+
+}
