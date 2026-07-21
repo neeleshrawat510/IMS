284181829ae8d9ac026e20a77de054c6f376e335
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-21 10:15:05 +0530

    debugging

--- edit_invoice.php
+++ edit_invoice.php
@@ -1,4 +1,9 @@
 <?php
+// Force PHP to display all errors and warnings
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 
 require_once "includes/auth_check.php";
 
