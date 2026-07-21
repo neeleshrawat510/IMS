88dc015da62274f15961c1d17b07f1baba8b1167
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-21 11:43:26 +0530

    fixed users tab

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -62,8 +62,8 @@
 
       <!-- USERS -->
       <a class="nav-link" href="setting.php">
-        <span class="nav-icon"><i class="bi bi-gear" aria-hidden="true"></i></span>
-        <span class="nav-text">Settings</span>
+        <span class="nav-icon"><i class="bi bi-people" aria-hidden="true"></i></span>
+        <span class="nav-text">Users</span>
       </a>
 
       <!-- USER DASHBOARD -->
