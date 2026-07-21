a2a618eaf8c88874f3b6460e96b07f33de965936
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-21 19:10:20 +0530

    removed redirection

--- payment_success.php
+++ payment_success.php
@@ -182,10 +182,10 @@ $invoice = mysqli_fetch_assoc($result);
                 Your transaction has been securely processed and your invoice has been updated.
             </div>
 
-            <a href="index.php" class="btn btn-primary px-4">
+            <!-- <a href="index.php" class="btn btn-primary px-4">
                 <i class="bi bi-house-door me-2"></i>
                 Back to Home
-            </a>
+            </a> -->
 
             <hr class="my-4">
 
