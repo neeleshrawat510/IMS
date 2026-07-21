b3c3afdb88775d09de9295ebbe36aecfe037fceb
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-21 11:12:51 +0530

    disable edit button if payment status is paid

--- php/manage_all_invoices.php
+++ php/manage_all_invoices.php
@@ -19,34 +19,38 @@ $sql = mysqli_query($conn, "
         ON invoices.contact_id = contacts.id
         WHERE invoices.remove = '0'
 ");
-$editButton = '';
 
-if ($row['payment_status'] != 'Paid') {
-    $editButton = '
-        <a href="edit_invoice.php?id=' . $row['id'] . '" class="btn btn-primary btn-sm" title="Edit">
-            <i class="bi bi-pencil"></i>
-        </a>';
-} else {
-    $editButton = '
-        <button class="btn btn-secondary btn-sm" title="Paid invoices cannot be edited" disabled>
-            <i class="bi bi-pencil"></i>
-        </button>';
-}
 
 $data = [];
 $sr = 1;
 if (mysqli_num_rows($sql) > 0) {
 
     while ($row = mysqli_fetch_array($sql)) {
-    $data[] = [
-                $row['id'],
-                $sr++,
-                $row['invoice_no'],
-                $row['name'],
-                $row['invoice_date'],
-                $row['grand_total'],
-                $row['payment_status'],
-                    '<a href="php/view_invoice.php?id=' . $row['id'] . '" target="_blank" class="btn btn-success btn-sm me-1" title="View">
+
+        //disable editing if payment status is paid
+        $editButton = '';
+
+        if ($row['payment_status'] != 'Paid') {
+            $editButton = '
+        <a href="edit_invoice.php?id=' . $row['id'] . '" class="btn btn-primary btn-sm" title="Edit">
+            <i class="bi bi-pencil"></i>
+        </a>';
+        } else {
+            $editButton = '
+        <button class="btn btn-secondary btn-sm" title="Paid invoices cannot be edited" disabled>
+            <i class="bi bi-pencil"></i>
+        </button>';
+        }
+
+        $data[] = [
+            $row['id'],
+            $sr++,
+            $row['invoice_no'],
+            $row['name'],
+            $row['invoice_date'],
+            $row['grand_total'],
+            $row['payment_status'],
+            '<a href="php/view_invoice.php?id=' . $row['id'] . '" target="_blank" class="btn btn-success btn-sm me-1" title="View">
                         <i class="bi bi-eye"></i> 
                     </a>
 
@@ -57,7 +61,7 @@ if (mysqli_num_rows($sql) > 0) {
                     </a>
                     <a href="#" class="btn btn-danger btn-sm delete-btn" title="Delete" data-id="' . $row['id'] . '">                          <i class="bi bi-trash"></i>
                     </a>'
-                ];
+        ];
     }
 
 }
