0b8a32882e885adff37daadfbe12496ed62ce45b
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-22 14:48:18 +0530

    doc formatting

--- php/manage_all_invoices.php
+++ php/manage_all_invoices.php
@@ -61,8 +61,7 @@ if (mysqli_num_rows($sql) > 0) {
                     </a>    
                     <a href="#"
                         data-id="' . $row['id'] . '"
-                        class="btn btn-warning btn-sm me-1 send-invoice-btn"
-                        title="Send Invoice">
+                        class="btn btn-warning btn-sm me-1 send-invoice-btn" title="Send Invoice">
                         <i class="bi bi-envelope"></i>
                     </a>
                     <a href="#" class="btn btn-danger btn-sm delete-btn" title="Delete" data-id="' . $row['id'] . '">                          <i class="bi bi-trash"></i>
