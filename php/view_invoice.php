5eb2f28e9fd5a974dc4d3dd9a6c90c2ac098efd2
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-23 10:13:12 +0530

    debugging

--- php/view_invoice.php
+++ php/view_invoice.php
@@ -1,31 +1,21 @@
 <?php
 
 require_once "../includes/api_auth.php";
-include("../config/connection.php");
+require_once "../config/connection.php";
+require_once "../controller/invoice_pdf.php";
 
 
 if (!isset($_GET['id'])) {
     die("Invalid request");
 }
 
-$id = intval($_GET['id']);
+$id = (int)$_GET['id'];
 
-$result = mysqli_query($conn, "SELECT pdf_path FROM invoices WHERE id = $id");
-$row = mysqli_fetch_assoc($result);
-
-if (!$row || empty($row['pdf_path'])) {
-    die("PDF not found");
-}
-
-$file = "../" . $row['pdf_path'];
-
-if (!file_exists($file)) {
-    die("File missing on server");
-}
+$pdf = generateInvoicePdf($conn, $id);
 
 header("Content-Type: application/pdf");
 header("Content-Disposition: inline; filename=\"invoice.pdf\"");
+header("Content-Length: " . strlen($pdf));
 
-readfile($file);
+echo $pdf;
 exit;
-?>
--- test_invoice.php
+++ test_invoice.php
@@ -1,6 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 include "config/connection.php";
 require_once "controller/invoice_pdf.php";
 
-echo generateInvoicePDF($conn, 29);
+echo generateInvoicePDF($conn, 1);
