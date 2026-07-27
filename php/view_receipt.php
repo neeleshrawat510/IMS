7266925617b8de8224d7d1c050ba6161a2133d9f
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-27 13:09:11 +0530

    fixed id

--- php/view_receipt.php
+++ php/view_receipt.php
@@ -12,8 +12,7 @@ if (!isset($_GET['id'])) {
 $invoiceId = (int)$_GET['id'];
 
 $pdf = generateReceiptPDF($conn, $invoiceId);
-var_dump($pdf);
-exit;
+
 if ($pdf === false) {
     die("Invoice not found");
 }
