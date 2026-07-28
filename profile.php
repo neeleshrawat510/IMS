f55e56e61ee938af5f81832e16f0145980ca2f8e
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-28 10:59:50 +0530

    fixed title

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ $id = intval($_GET['id']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Invoice management System">
-    <title>Manage Contacts | Invoice Management System</title>
+    <title>Profile | Invoice Management System</title>
 
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
