93008cc26b6dd76f91975bb571244de6da0337bd
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-31 09:43:09 +0530

    debugging

--- php/dashboard_user.php
+++ php/dashboard_user.php
@@ -1,5 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once "../includes/api_auth.php";
+include("../config/connection.php");
 require_once "../includes/api_auth.php";
 include("../config/connection.php");
 
