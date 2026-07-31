982b5ab26655688b43aa36cbc221509f19856695
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-07-31 10:12:51 +0530

    removed bundle.js

--- user_profile.php
+++ user_profile.php
@@ -18,8 +18,6 @@ require_once "includes/auth_check.php";
 
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.8/css/dataTables.dataTables.min.css">
 
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- jQuery  -->
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
