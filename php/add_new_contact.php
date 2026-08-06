3c24ca2508b5cc444ff84e1e73a94d05bd47f3b2
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-06 08:59:57 +0530

    store hubSpot contact id

--- php/add_new_contact.php
+++ php/add_new_contact.php
@@ -4,12 +4,12 @@ require_once "../includes/api_auth.php";
 include("../config/connection.php");
 require_once "../includes/HubSpotService.php";
 
-$name = trim(mysqli_real_escape_string($conn, $_POST['name']  ??  ''));
-$number = trim(mysqli_real_escape_string($conn, $_POST['number']  ??  ''));
-$email = trim(mysqli_real_escape_string($conn, $_POST['email']  ??  ''));
-$company = trim(mysqli_real_escape_string($conn, $_POST['company']  ??  ''));
-$gst = trim(mysqli_real_escape_string($conn, $_POST['gst']  ??  ''));
-$address = trim(mysqli_real_escape_string($conn, $_POST['address']  ??  ''));
+$name = trim(mysqli_real_escape_string($conn, $_POST['name'] ?? ''));
+$number = trim(mysqli_real_escape_string($conn, $_POST['number'] ?? ''));
+$email = trim(mysqli_real_escape_string($conn, $_POST['email'] ?? ''));
+$company = trim(mysqli_real_escape_string($conn, $_POST['company'] ?? ''));
+$gst = trim(mysqli_real_escape_string($conn, $_POST['gst'] ?? ''));
+$address = trim(mysqli_real_escape_string($conn, $_POST['address'] ?? ''));
 $todayDate = date('Y-m-d H:i:s');
 $created_by = $_SESSION['user_name'];
 
@@ -26,7 +26,7 @@ if ($insert) {
         $nameParts = explode(' ', trim($name), 2);
 
         $firstName = $nameParts[0];
-        $lastName  = $nameParts[1] ?? '';
+        $lastName = $nameParts[1] ?? '';
 
         $hubspotResponse = $hubspot->createContact(
             $firstName,
@@ -34,6 +34,22 @@ if ($insert) {
             $email,
             $number
         );
+
+        $hubspotId = $hubspotResponse['response']['id'] ?? NULL;
+
+        //update hubspot contact id in DB
+        if ($hubspotId) {
+
+            $contactId = mysqli_insert_id($conn);
+
+            mysqli_query(
+                $conn,
+                "UPDATE contacts
+         SET hubspot_contact_id = '$hubspotId'
+         WHERE id = '$contactId'"
+            );
+
+        }
 
         // Optional: log response for debugging
         // file_put_contents("hubspot.log", print_r($hubspotResponse, true), FILE_APPEND);
