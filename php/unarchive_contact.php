0c3bf89008afa8d1b112198d544879a4cacfcdce
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-07 09:57:28 +0530

    unarchive contacts

--- php/unarchive_contact.php
+++ php/unarchive_contact.php
@@ -1,16 +1,33 @@
 <?php
 
 require_once "../includes/api_auth.php";
+require_once "../includes/HubSpotService.php";
 include("../config/connection.php");
 
-$contactId = $_POST['id'];
+$contactId = intval($_POST['id']);
 
-$unarchive = mysqli_query($conn, "UPDATE `contacts` SET remove = 0 WHERE `id` = '$contactId'");
+$get = mysqli_query($conn, "SELECT hubspot_contact_id FROM contacts WHERE id='$contactId'");
+$row = mysqli_fetch_assoc($get);
 
-if($unarchive){
+$hubspotId = $row['hubspot_contact_id'] ?? null;
+
+$unarchive = mysqli_query($conn, "UPDATE contacts SET remove=0 WHERE id='$contactId'");
+
+if ($unarchive) {
+
+    if (!empty($hubspotId)) {
+        try {
+            $hubspot = new HubSpotService();
+            $hubspot->updateContactStatus($hubspotId, "Active");
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+
     echo "success";
-}else{
-    echo "failed";
-}
 
-?>
+} else {
+
+    echo "failed";
+
+}
