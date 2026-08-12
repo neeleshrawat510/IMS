80cc74067acdf2ee9e0065f44aec0c94d8029df3
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-12 13:03:06 +0530

    testing

--- test_zapier.php
+++ test_zapier.php
@@ -4,8 +4,8 @@ $webhookUrl = $_ENV['ZAPIER_WEBHOOK_URL'] ?? getenv('ZAPIER_WEBHOOK_URL');
 ;
 
 $data = [
-    "name" => "Neelesh",
-    "email" => "neelesh@example.com",
+    "name" => "Neelesh Test",
+    "email" => "neeleshtest@example.com",
     "phone" => "9876543210"
 ];
 
