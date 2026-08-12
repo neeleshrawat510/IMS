4d3dd9986e101fcb186530abc6c4028cbf821ecd
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-12 12:41:25 +0530

    test zapier

---
+++ test_zapier.php
@@ -0,0 +1,25 @@
+<?php
+
+$webhookUrl = $_ENV['ZAPIER_WEBHOOK_URL'] ?? getenv('ZAPIER_WEBHOOK_URL');
+;
+
+$data = [
+    "name" => "Test User",
+    "email" => "test@example.com",
+    "phone" => "9876543210"
+];
+
+$ch = curl_init($webhookUrl);
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+echo $response;
+
+curl_close($ch);
