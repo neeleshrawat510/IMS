1bdecff9d0dcf34b515682e037e80174e9df04fb
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-12 13:18:43 +0530

    testing

--- test_zapier.php
+++ test_zapier.php
@@ -4,8 +4,8 @@ $webhookUrl = $_ENV['ZAPIER_WEBHOOK_URL'] ?? getenv('ZAPIER_WEBHOOK_URL');
 ;
 
 $data = [
-    "name" => "Akash Test ",
-    "email" => "akashverma9780@gmail.com",
+    "name" => "Akash 1 Test ",
+    "email" => "aakashverma9780@gmail.com",
     "phone" => "9876543210"
 ];
 
