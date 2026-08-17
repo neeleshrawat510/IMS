3aed67c5945527e950a5b283c3a7101e06b5bd50
Neelesh Rawat <neeleshrawat510@gmail.com>
2026-08-17 16:26:26 +0530

    fixed

--- php/archive_product.php
+++ php/archive_product.php
@@ -29,7 +29,7 @@ if (!empty($hubspotProductId)) {
 
         $hubspotResponse = $hubspot->updateProductStatus(
             $hubspotProductId,
-            "Inactive"
+            "inactive"
         );
         error_log(
     "HubSpot Product Status Response: " .
